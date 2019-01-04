66cf0c9c215402011640d001186bd35171600099
Ryan Gill <rdgill95@gmail.com>
2019-01-04 01:34:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,6 +50,7 @@ def all_supplies_in_holidays(holiday_hash)
     holiday.each do |holiday, supplies|
       supplies = supplies.join (", ")
       holiday = holiday.to_s.split("_").find {|x| x.capitalize}.join(" ")<< ":"
+      puts " "
   end
 
 end
